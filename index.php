f92c73530c01f480b5a1100db582c70b790ef6a4
mohammaddanish-cedcoss <mohddanish@makewebbetter.com>
2021-01-02 19:15:17 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php echo hello; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<h3>Hi This is index Page</h3>
+	</body>
+</html>
